0f831df98e723d878c5086b39bcac25354972043
chachahana <soameren1@gmail.com>
2020-12-02 15:40:39 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { Fragment } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <div className="c1_box"></div>
+      <div>
+        <h2>CHANAS LAB</h2>
+      </div>
+      <div>
+        <h3>We support your wealth and life.</h3>
+      </div>
+      <div className="c1_box"></div>
+      <div id="stylefive">
+        <ul>
+          <li><a>cha</a></li>
+          <li><a>na</a></li>
+          <li><a>sss</a></li>
+        </ul>
+      </div>
+      <div><h1>chanas</h1></div>
+    </Fragment>
+  );
+}
+
+export default App;
